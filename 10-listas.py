aa8cddc3e60b7fdcf685e4a48f4e51679c8f1f64
Diego Ferreira <diego_fsilva92@hotmail.com>
2023-10-24 21:53:57 -0300

    listas

---
+++ 10-listas.py
@@ -0,0 +1,18 @@
+gameFifa = ["Fifa 23", 2022, 90.50, True]
+print(gameFifa)
+
+gameList = ["Resident Evil", "Red Dead Redemption 2", 
+            "The Legend of Zelda", "Baldurs Gate 3"]
+print(gameList)
+
+#1 - Buscando os dois primeiros itens da lista
+print(gameList[0:2])
+
+#2 Buscar o último item da lista
+print(gameList[-1])
+
+#3 - Buscar jogos até determinada posição
+print(gameList[:3])
+
+#4 - Buscar jogos de uma posição em diante
+print(gameList[1:])
