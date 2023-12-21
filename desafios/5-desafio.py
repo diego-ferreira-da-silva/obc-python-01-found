3c794e4b056ecad78b9918215cc88e6212d745e3
Diego Ferreira <diego_fsilva92@hotmail.com>
2023-12-21 13:38:56 -0300

    Organização do projeto e início do desafio final

--- desafios/5-desafio.py
+++ desafios/5-desafio.py
@@ -17,6 +17,24 @@ def contLetter(frase):
 
 contLetter("Teste de Frase")
 
+"""
+Abaixo a resolução criada pelo Professor
+def check_char(text):
+    type={"Uppercase":0, "Lowercase":0}
+    for char in text:
+        if char.isupper():
+           type["Uppercase"]+=1
+        elif char.islower():
+           type["Lowercase"]+=1
+    print ("Texto original: ", text)
+    print ("Número de letras maiúsculas: ", type["Uppercase"])
+    print ("Número de letras minúsculas: ", type["Lowercase"])
+
+#string_test('The quick Brown Fox')
+check_char("A melhor forma de prever o futuro é Criá-Lo")
+
+"""
+
 ## Lista números pares e ímpares de uma lista
 #Escreva uma função Python para imprimir os números pares e ímpares em duas listas separadas para cada uma.
 
@@ -34,3 +52,18 @@ def parImpar(listaNumeros):
   print(f"Separação de números ímpares e pares da lista: {listaNumeros} \nPares: {par} \nImpares: {impar}")
   
 parImpar([2, 3, 4, 5])
+
+"""
+Abaixo a resolução criada pelo professor
+
+def check_numbers(numbers):
+    pairs = []
+    odd = []
+    for number in numbers:
+        if number % 2 == 0:
+            pairs.append(number)
+        else:
+            odd.append(number)
+    return pairs, odd
+print(check_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+"""
